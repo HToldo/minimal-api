c69bb0a377b0e207e43ffcc51944e91591dfcd95
Henrique Toldo <henrique-toldo@outlook.com>
2025-09-08 20:00:31 -0300

    Iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using MinimalApi.DTOs;
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Password == "123456")
+        return Results.Ok("Login com sucesso");
+    else return Results.Unauthorized();    
+});
+    
+
+app.Run();
+
